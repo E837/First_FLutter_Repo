e668340451f06e342d974ccb00391fdf89eb7fd9
Ehsan <e.moradi274@gmail.com>
2020-10-28 22:53:01 +0330

    session7 "StatelessWidget and hotReload"

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('this is my first appBar'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text(
-        'hello world!',
-        style: TextStyle(
-          color: Colors.orange[600],
-          fontSize: 40,
-          fontFamily: 'segoeBold',
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('this is my first appBar'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'hello world!',
+          style: TextStyle(
+            color: Colors.orange[600],
+            fontSize: 40,
+            fontFamily: 'lalezar',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('+'),
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        child: Text('+'),
+      ),
+    );
+  }
+}
+

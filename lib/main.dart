55d6ea26899a6abac716b9b39fd44e386de5cf68
Ehsan <e.moradi274@gmail.com>
2020-10-31 11:03:21 +0330

    session9 "Button and Icon"

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,17 @@ class Home extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Image.network('https://havinchap.com/wp-content/uploads/2019/06/%DA%86%D8%A7%D9%BE-%D8%B1%D9%88%DB%8C-%D8%A7%D8%AC%D8%B3%D8%A7%D9%85-%DA%86%D8%A7%D9%BE-%D8%B1%D9%88%DB%8C-%D8%AA%DB%8C-%D8%B4%D8%B1%D8%AA-%DB%8C%D9%82%D9%87-%DA%AF%D8%B1%D8%AF-%DA%86%D8%A7%D9%BE-%D8%B1%D9%88%DB%8C-%D9%84%D8%A8%D8%A7%D8%B3-%DA%86%D8%A7%D9%BE-%D8%B1%D9%88%DB%8C-%D8%AA%DB%8C-%D8%B4%D8%B1%D8%AA-%D8%AA%DB%8C%D8%B4%D8%B1%D8%AA-%DA%86%D8%A7%D9%BE-%D9%84%D9%88%DA%AF%D9%88-%D8%B1%D9%88%DB%8C-%D8%AA%DB%8C%D8%B4%D8%B1%D8%AA-%D8%AA%DB%8C%D8%B4%D8%B1%D8%AA-%D8%A7%D8%AE%D8%AA%D8%B5%D8%A7%D8%B5%DB%8C.png'),
+        // child: Image.asset('assets/2621720.jpg'),
+        child: RaisedButton.icon(
+          onPressed: () {},
+          color: Colors.amber,
+          label: Text(
+            'its a button',
+          ),
+          icon: Icon(
+            Icons.mail,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('+'),

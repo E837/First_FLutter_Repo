cc63668e95019a13dc353b7d8008ba25d5a4390b
Ehsan <e.moradi274@gmail.com>
2020-11-01 13:17:31 +0330

    session12 "Columns (plus rows and containers"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -12,23 +13,51 @@ class Home extends StatelessWidget {
         title: Text('this is my first appBar'),
         centerTitle: true,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('part 1'),
-          RaisedButton.icon(
-            onPressed: () {},
-            label: Text('part 2'),
-            color: Colors.amber,
-            icon: Icon(Icons.mail),
-          ),
-          Container(
-            color: Colors.deepPurpleAccent,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('part 3'),
-          ),
-        ],
+      body: Container(
+        margin: EdgeInsets.only(bottom: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('part 1'),
+                  RaisedButton.icon(
+                    onPressed: () {},
+                    label: Text('part 2'),
+                    color: Colors.amber,
+                    icon: Icon(Icons.mail),
+                  ),
+                  Container(
+                    color: Colors.deepPurpleAccent,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Text('part 3'),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.only(top: 40),
+            ),
+            Container(
+              color: Colors.teal,
+              child: Text('row 2'),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              margin: EdgeInsets.only(left: 120),
+            ),
+            Container(
+              color: Colors.pink,
+              child: Text('row 3'),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            ),
+            Container(
+              color: Colors.redAccent,
+              child: Text('row 4'),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              margin: EdgeInsets.only(right: 120),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('+'),

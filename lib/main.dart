dbc970cdc75dacebc2ee16a01b615b0f9300d563
Ehsan <e.moradi274@gmail.com>
2020-11-10 02:27:13 +0330

    session19 "Cards"

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,35 @@ class _HomeState extends State<Home> {
     Quote(author:'author 3', text:'text 3'),
   ];
 
+  Widget cardsTemplate (Quote quote) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                  color: Colors.blue
+              ),
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +62,7 @@ class _HomeState extends State<Home> {
       body: Padding(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
         child: Column(
-          children: sampleQuotes.map((quote) => Text('${quote.author} - ${quote.text}')).toList(),
+          children: sampleQuotes.map((quote) => cardsTemplate(quote)).toList(),
           // --> the traditional and classic way is commented below
           // children: sampleQuotes.map((quote) {
           //   return Text('${quote.author}-${quote.text}');
